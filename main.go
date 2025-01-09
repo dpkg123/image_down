77fc379790176c393785cb64b2f8e5e494ca42bb
Ayayoku Togetsu <130299352+dpkg123@users.noreply.github.com>
2025-01-10 02:05:02 +0800

    更新 main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 const (
     concurrentDownloads = 50
     userAgent          = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.2903.9"
-    bufferSize        = 64 * 1024 // 64KB buffer
+    bufferSize        = 256 * 1024 // 64KB buffer
 )
 
 var urls = []string{
